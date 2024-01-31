e717c2881b70f2b05ae744c8597ffd0d93e7c190
Юлия <156711500+qayumonte@users.noreply.github.com>
2024-01-31 14:33:33 +0100

    Add files via upload

---
+++ lesson1/lesson_1_task_7.py
@@ -0,0 +1,41 @@
+def one():
+    print(1)
+
+def two():
+    print(2)
+
+def three():
+    print(3)
+
+def four():
+    print(4)
+
+def five():
+    print(5)
+
+def six():
+    print(6)
+
+def seven():
+    print(7)
+
+def eight():
+    print(8)
+
+def nine():
+    print(9)
+
+def zero():
+    print(0)
+
+eight()
+eight()
+zero()
+zero()
+five()
+five()
+five()
+three()
+five()
+three()
+five()
